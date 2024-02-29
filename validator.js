44ef66458ab0102d0cafc05a8abcf45d1f115a27
emerson souza <emerson-santos1940@hotmail.com>
2024-02-29 20:16:49 -0300

    Update Index

--- validator.js
+++ validator.js
@@ -1,3 +1,30 @@
-function submit() {
-  window.onload("index.php");
-}
+formulario = document.getElementById("formulario");
+email = document.getElementById("email");
+senha = document.getElementById("senha");
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //Verifica se o nome está vazio
+  if (email.value === "" || isEmailValid(email.value)) {
+    alert("O email está vazio");
+    return;
+  }
+
+  function isEmailValid(email) {
+    const emailRegex = new RegExp(
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if (emailRegex.test(email)) {
+      console.log("ok");
+    }
+
+    if (senha.value === "") {
+      alert("A senha está vazio");
+      return;
+    }
+
+    formulario.submit();
+  }
+});
